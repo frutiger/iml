66eb4a61ac555e1ed56195727442c99253484ddb
Masud Rahman <masud.s.rahman@gmail.com>
2012-11-16 01:24:49 -0500

    Introduce ability to simply instantiate and add children

---
+++ iml.js
@@ -0,0 +1,18 @@
+var IML = {
+    instantiate: function(node, meta) {
+        console.log(node);
+        if (!meta[node.tagName]) {
+            return;
+        }
+
+        var metadata = meta[node.tagName];
+
+	var object = meta[node.tagName].create();
+        for (var i = 0; i < node.childNodes.length; ++i) {
+            meta[node.tagName].child(object,
+				     IML.instantiate(node.childNodes[i], meta));
+        }
+        return object;
+    }
+}
+
