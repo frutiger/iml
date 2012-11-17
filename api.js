1ec35b98c388ff90148403a1c9049a9d39814292
Masud Rahman <masud.s.rahman@gmail.com>
2012-11-17 05:59:39 -0500

    Remove trailing whitespace

--- api.js
+++ api.js
@@ -31,12 +31,12 @@ var API = {
                 create: function() {
                     return new API.Container();
                 },
-    
+
                 child: function(self, child) {
                     self.addChild(child);
                 }
             },
-    
+
             Child: {
                 create: function() {
                     return new API.Child();
