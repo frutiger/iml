43a75dc71fb5a459c9e3b1d43c7139f585f66df1
Masud Rahman <masud.s.rahman@gmail.com>
2012-11-17 06:46:49 -0500

    Enumerate and uniquely identify each object instance created of each type

--- api.js
+++ api.js
@@ -2,35 +2,44 @@ var log;
 
 var API = {
     Container: function() {
+        var s_id = 0;
+
         function Container() {
-            log("Creating a 'Container'");
+            this.id = s_id++;
             this.children = [];
+            log("Creating " + this);
         }
 
         Container.prototype.addChild = function(child) {
-            log("Adding a child to 'Container'");
             this.children.push(child);
+            log("Adding " + child + " to " + this);
+        };
+
+        Container.prototype.toString = function() {
+            return '"Container' + this.id + '"';
         };
 
         Object.defineProperty(Container.prototype, "property", {
             set: function(value) {
-                log("Setting 'Container.property' to " + value);
+                log('Setting "property" on ' + this + ' to ' + value);
                 this.d_property = value;
             },
-            get: function() {
-                log("Getting 'Container.property'");
-                return this.d_property;
-            },
-            enumerable: true,
         });
 
         return Container;
     }(),
 
     Child: function() {
+        var s_id = 0;
+
         function Child() {
-            log("Creating a 'Child'");
+            this.id = s_id++;
+            log("Creating " + this);
         }
+
+        Child.prototype.toString = function() {
+            return '"Child' + this.id + '"';
+        };
 
         return Child;
     }(),
